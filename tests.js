74089b34af462bd85614b4cf95142728ffa7c990
David Ernst <git@dsernst.com>
2015-09-01 17:05:08 -0700

    Add placeholder tests for argument checking

--- tests.js
+++ tests.js
@@ -2,3 +2,5 @@ var triples = require('./index.js');
 
 console.log(triples.euclid(2, 1)); // [3, 4, 5]
 console.log(triples.euclid(3, 2)); // [5, 12, 13]
+console.log(exports.euclid(0, 1)); // throw TypeError('m and n must not be less than 1')
+console.log(exports.euclid(2, 2)); // throw TypeError('m must not equal n')
