d68de3fd488614f2395a9892eb06d1fd6a296f1f
David Ernst <git@dsernst.com>
2015-09-01 17:02:56 -0700

    Add argument checking for m,n < 1 or m = n

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 exports.euclid = function (m, n) {
+  if (m < 1 || n < 1) {
+    throw new TypeError('m and n must not be less than 1');
+  }
+  if (m === n) {
+    throw new TypeError('m must not equal n');
+  }
   var a = Math.abs(m * m - n * n);
   var b = 2 * m * n;
   var c = m * m + n * n;
